aa5e9140e3ce8a04bcbbe2d5c2eb1ad136882c99
John <68535837+John-D-S@users.noreply.github.com>
2021-11-14 20:12:43 +1100

    wrote level generation for offline scenes

--- Assets/RTAC/04_Scripts/Environment/LevelSection.cs
+++ Assets/RTAC/04_Scripts/Environment/LevelSection.cs
@@ -6,7 +6,19 @@ using UnityEngine;
 public class LevelSection : MonoBehaviour
 {
     [SerializeField] private List<Sphere> localSpheres = new List<Sphere>();
+    [SerializeField] private PosRot nextPosRot = new PosRot();
+    public PosRot NextPosRot;
 
+    public List<Sphere> SpheresWhenSectionAtPosRot(PosRot _posRot)
+    {
+        List<Sphere> returnVal = new List<Sphere>();
+        foreach(Sphere sphere in localSpheres)
+        {
+            returnVal.Add(new Sphere(_posRot.position + _posRot.Rotation * (_posRot.position + sphere.position), sphere.radius));
+        }
+        return returnVal;
+    }
+    
     public List<Sphere> GlobalSpheres
     {
         get
@@ -20,5 +32,11 @@ public class LevelSection : MonoBehaviour
         }
     }
     
-    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector3 spawnGizmoPos = transform.position + transform.rotation * nextPosRot.position;
+        Gizmos.DrawSphere(spawnGizmoPos, 0.25f);
+        Gizmos.DrawLine(spawnGizmoPos, transform.rotation * nextPosRot.Rotation * Vector3.forward + spawnGizmoPos);
+    }
 }
